f7029acbe1e3feb316f6706844840716b8949b6c
RoyAzran <royazran177@gmail.com>
2025-08-25 21:36:04 +0300

    Update ec2.py

--- ec2.py
+++ ec2.py
@@ -76,6 +76,8 @@ def start_instance(instance_id):
     ec2 = boto3.client('ec2')
     response = ec2.describe_instances(InstanceIds=[instance_id])
     state = response['Reservations'][0]['Instances'][0]['State']['Name']
+    if state == 'stopping':
+         print(f"Instance {instance_id} is in 'stopping' state , wait 10 seconds please :) ")
     if state == 'running':
         print(f"Instance {instance_id} is already in 'running' state")
     elif state == 'pending':
@@ -83,4 +85,5 @@ def start_instance(instance_id):
     else:
         ec2.start_instances(InstanceIds=[instance_id])
         print(f"Start requested for {instance_id} ")
+
 
